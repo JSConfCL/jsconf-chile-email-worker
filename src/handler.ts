7163ec6a310cf887900c7f13e4fa4c12dd10c66d
Felipe Torres <ftorres@brex.com>
2022-03-29 20:12:42 -0700

    update

---
+++ src/handler.ts
@@ -0,0 +1,41 @@
+const createAirtableRecord = (body: Record<string, any>) => {
+  return fetch(
+    `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${encodeURIComponent(
+      AIRTABLE_TABLE_NAME,
+    )}`,
+    {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+        'Content-type': `application/json`,
+      },
+    },
+  )
+}
+
+function simpleResponse(statusCode: number, message: string) {
+  return new Response(
+    JSON.stringify({
+      message: message,
+      status: statusCode,
+    }),
+    {
+      headers: { 'Content-Type': 'application/json' },
+      status: statusCode,
+    },
+  )
+}
+
+export async function handleRequest(request: Request): Promise<Response> {
+  const body = await request.formData()
+  const email = Object.fromEntries(body)
+
+  try {
+    await createAirtableRecord({ records: [{ fields: { emails: email } }] })
+    return simpleResponse(200, 'ok')
+  } catch (e) {
+    console.error(e)
+    return simpleResponse(400, 'error')
+  }
+}
