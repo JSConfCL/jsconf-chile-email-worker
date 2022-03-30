bd1d8c5d8204432f6a7895e6aed2ceb02f4bbbee
Felipe Torres <ftorres@brex.com>
2022-03-29 20:40:10 -0700

    update

--- src/handler.ts
+++ src/handler.ts
@@ -14,11 +14,49 @@ const createAirtableRecord = (body: Record<string, any>) => {
   )
 }
 
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+  'Access-Control-Max-Age': '86400',
+}
+function handleOptions(request: Request) {
+  // Make sure the necessary headers are present
+  // for this to be a valid pre-flight request
+  const headers = request.headers
+  if (
+    headers.get('Origin') !== null &&
+    headers.get('Access-Control-Request-Method') !== null &&
+    headers.get('Access-Control-Request-Headers') !== null
+  ) {
+    // Handle CORS pre-flight request.
+    // If you want to check or reject the requested method + headers
+    // you can do that here.
+    const respHeaders = {
+      ...corsHeaders,
+      // Allow all future content Request headers to go back to browser
+      // such as Authorization (Bearer) or X-Client-Name-Version
+      'Access-Control-Allow-Headers': request.headers.get(
+        'Access-Control-Request-Headers',
+      ) as string,
+    }
+    return new Response(null, {
+      headers: respHeaders,
+    })
+  } else {
+    // Handle standard OPTIONS request.
+    // If you want to allow other HTTP Methods, you can do that here.
+    return new Response(null, {
+      headers: {
+        Allow: 'GET, HEAD, POST, OPTIONS',
+      },
+    })
+  }
+}
+
 function simpleResponse(statusCode: number, message: string) {
   const responseHeaders = new Headers()
   responseHeaders.set('Access-Control-Allow-Origin', '*')
   responseHeaders.set('Content-Type', 'application/json')
-
   return new Response(
     JSON.stringify({
       message: message,
@@ -32,14 +70,21 @@ function simpleResponse(statusCode: number, message: string) {
 }
 
 export async function handleRequest(request: Request): Promise<Response> {
-  const body = await request.formData()
-  const email = Object.fromEntries(body)
-
-  try {
-    await createAirtableRecord({ records: [{ fields: { emails: email } }] })
-    return simpleResponse(200, 'ok')
-  } catch (e) {
-    console.error(e)
-    return simpleResponse(400, 'error')
+  if (request.method === 'OPTIONS') {
+    return handleOptions(request)
+  } else {
+    try {
+      const { email } = (await request.json()) as {
+        email: string
+      }
+      if (!email) {
+        throw new Error('Missing email')
+      }
+      await createAirtableRecord({ records: [{ fields: { emails: email } }] })
+      return simpleResponse(200, 'ok')
+    } catch (e) {
+      console.error(e)
+      return simpleResponse(400, 'error')
+    }
   }
 }
